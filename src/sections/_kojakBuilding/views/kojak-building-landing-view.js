49faa61645f81afe39ed1041d2c8cb73a8f6442f
Hussam <hussam@hotmail.co.uk>
2023-07-25 15:23:59 +0400

    change case

--- src/sections/_kojakBuilding/views/kojak-building-landing-view.js
+++ src/sections/_kojakBuilding/views/kojak-building-landing-view.js
@@ -2,7 +2,7 @@ import FAQs from 'src/sections/_kojakBuilding/landing/Faqs';
 import ContactUs from 'src/sections/_kojakBuilding/landing/ContactUs';
 import WhatWeOffer from 'src/sections/_kojakBuilding/landing/WhatWeOffer';
 import WhyKojakBuilding from 'src/sections/_kojakBuilding/landing/WhyKojakBuilding';
-import KojakBuildingLandingHero from 'src/sections/_kojakBuilding/landing/KojakBuildingLandingHero';
+import KojakBuildingLandingHero from 'src/sections/_kojakBuilding/landing/kojakBuildingLandingHero';
 
 export default function KojakBuildingLandingView() {
   return (
