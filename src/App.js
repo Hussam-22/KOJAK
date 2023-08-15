b4f89fbfd7ab17e303b90f949aae9f873d8f2bb4
Hussam Al Khudari <hussam@hotmail.co.uk>
2023-08-16 00:46:36 +0400

    update without fixing the reload of translation

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { LocalizationProvider } from 'src/locales';
 import ProgressBar from 'src/components/progress-bar';
 import MotionLazy from 'src/components/animate/motion-lazy';
 import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
-import { SettingsDrawer, SettingsProvider } from 'src/components/settings';
+import { ThemeSettings, SettingsDrawer, SettingsProvider } from 'src/components/settings';
 
 // ----------------------------------------------------------------------
 
@@ -27,18 +27,20 @@ export default function App() {
       <ReduxProvider store={store}>
         <LocalizationProvider>
           <SettingsProvider
-            defaultSettings={{
-              themeMode: 'light', // 'light' | 'dark'
-              themeDirection: 'ltr', //  'rtl' | 'ltr'
-              themeColorPresets: 'preset04', // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
-            }}
+          // defaultSettings={{
+          //   themeMode: 'light', // 'light' | 'dark'
+          //   themeDirection: 'ltr', //  'rtl' | 'ltr'
+          //   themeColorPresets: 'preset04', // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
+          // }}
           >
             <ThemeProvider>
-              <MotionLazy>
-                <ProgressBar />
-                <SettingsDrawer />
-                <Router />
-              </MotionLazy>
+              <ThemeSettings>
+                <MotionLazy>
+                  <ProgressBar />
+                  <SettingsDrawer />
+                  <Router />
+                </MotionLazy>
+              </ThemeSettings>
             </ThemeProvider>
           </SettingsProvider>
         </LocalizationProvider>
