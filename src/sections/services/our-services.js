dc21012bbb599cd08bbc29399a410cb4e19e8e3d
Hussam <hussam@hotmail.co.uk>
2023-08-30 14:47:21 +0400

    Change ServicesHowItWork order on mobile

--- src/sections/services/our-services.js
+++ src/sections/services/our-services.js
@@ -1,16 +1,17 @@
+import { Box, Container } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
-import { Box, useTheme, Container } from '@mui/material';
 
 import { _autoRepairServices } from 'src/_mock';
+import { useResponsive } from 'src/hooks/use-responsive';
 import ServiceItem from 'src/sections/components/service-item';
 import ServicesHowItWork from 'src/sections/services/how-it-works';
 
 export default function OurServices() {
-  const theme = useTheme();
+  const mdUp = useResponsive('up', 'md');
   return (
     <Container maxWidth="xl" sx={{ pb: 8 }}>
       <Grid container spacing={4}>
-        <Grid md={8} xs={12}>
+        <Grid md={8} xs={12} sx={{ order: mdUp ? 1 : 2 }}>
           <Box
             sx={{
               display: 'grid',
@@ -42,7 +43,7 @@ export default function OurServices() {
               ))}
           </Box>
         </Grid>
-        <Grid md={4} xs={12} sx={{ mt: 2 }}>
+        <Grid md={4} xs={12} sx={{ mt: 2, order: mdUp ? 2 : 1 }}>
           <ServicesHowItWork />
         </Grid>
       </Grid>
