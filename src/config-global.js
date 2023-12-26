aef0a9438c234842c68581b2bbcb47d272088f28
Hussam-22 <hussam@hotmail.co.uk>
2023-12-26 07:41:24 +0400

    Fix Footer URLs

--- src/config-global.js
+++ src/config-global.js
@@ -7,8 +7,8 @@ export const CART_FORM = 'CART';
 export const PAGE_VISIT = 'PAGE_VISIT';
 export const OUT_OF_STOCK = 'OUT_OF_STOCK';
 
-export const AUTO_URL = 'http://kojak-auto-maintenance.com/';
-export const SPARE_PART_URL = 'https://kojak-spare-parts-staging.netlify.app/';
+export const AUTO_URL = 'https://kojak-auto-maintenance.com/';
+export const SPARE_PART_URL = 'https://kojak-spareparts/';
 export const EXCLUSIVE_URL = 'https://kojak-exclusive.com/';
 export const BUILDING_URL = 'https://kojak-building.com/';
 export const GROUP_URL = 'https://kojakgroup.com/';
